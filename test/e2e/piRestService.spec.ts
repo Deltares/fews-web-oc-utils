349d96c8ecb1cfc0fd67d086710f36465de954f7
Rudie Ekkelenkamp <ekkelenkamp@gmail.com>
2022-12-21 20:12:10 +0100

    support request option for relative urls.

--- test/e2e/piRestService.spec.ts
+++ test/e2e/piRestService.spec.ts
@@ -1,5 +1,5 @@
 import 'cross-fetch/polyfill';
-import {DefaultParser, PiRestService} from "../../src";
+import {DefaultParser, PiRestService, RequestOptions} from "../../src";
 import DataRequestResult from "../../src/restservice/dataRequestResult";
 
 const baseUrl = process.env.TEST_URL || "";
@@ -7,15 +7,23 @@ const baseUrl = process.env.TEST_URL || "";
 describe("pi rest service", function () {
     it("get locations", async function () {
         const provider = new PiRestService(baseUrl);
-        const res = await provider.getData<DataRequestResult<any>>(baseUrl + "/rest/fewspiservice/v1/locations?documentFormat=PI_JSON");
+        const res = await provider.getData<DataRequestResult<unknown>>(baseUrl + "/rest/fewspiservice/v1/locations?documentFormat=PI_JSON");
         expect(res.data).not.toBeNull()
 
     })
-    it("get locations with default parser", async function () {
+    it("get locations with default parser and absolute url", async function () {
         const provider = new PiRestService(baseUrl);
-        const res = await provider.getDataWithParser<DataRequestResult<any>>(baseUrl + "/rest/fewspiservice/v1/locations?documentFormat=PI_JSON", new DefaultParser());
+        const requestOptions = new RequestOptions();
+        requestOptions.relativeUrl = false
+        const res = await provider.getDataWithParser<DataRequestResult<unknown>>(baseUrl + "/rest/fewspiservice/v1/locations?documentFormat=PI_JSON", requestOptions, new DefaultParser());
         expect(res.data).not.toBeNull()
+    })
+    it("get locations with default parser and relative url", async function () {
+        const provider = new PiRestService(baseUrl);
+        const requestOptions = new RequestOptions();
+        const res = await provider.getDataWithParser<DataRequestResult<unknown>>(  "/rest/fewspiservice/v1/locations?documentFormat=PI_JSON", requestOptions, new DefaultParser());
+        expect(res.data).not.toBeNull()
+    })
 
-    });
 
 })
