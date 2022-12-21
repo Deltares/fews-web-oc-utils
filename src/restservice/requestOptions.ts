be886411dae4f4a14d94d867e31a7d44bca5e13e
Rudie Ekkelenkamp <ekkelenkamp@gmail.com>
2022-12-21 19:24:14 +0100

    improve code reuse.

--- src/restservice/requestOptions.ts
+++ src/restservice/requestOptions.ts
@@ -1,3 +1,22 @@
-export default interface RequestOptions {
-    cache: string;
+export class RequestOptions {
+    private _mode = "cors";
+    private _relativeUrl = true;
+
+
+    get relativeUrl(): boolean {
+        return this._relativeUrl;
+    }
+
+    set relativeUrl(value) {
+        this._relativeUrl = value;
+    }
+
+    get mode(): string {
+        return this._mode;
+    }
+
+    set mode(value: string) {
+        this._mode = value;
+    }
+
 }
