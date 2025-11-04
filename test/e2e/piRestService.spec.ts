3b654d6a03e3f210085558b5b3817b4c5437cb20
Werner Kramer <werner.kramer@deltares.nl>
2025-11-04 13:32:42 +0100

    refactor: update package.json for module exports and build process
    - Changed module entry point to "./dist/fews-web-oc-utils.js".
    - Updated build scripts to use TypeScript and Vite.
    - Adjusted devDependencies to include Vite and Vitest.
    - Removed unused dependencies and updated Node engine requirement.
    
    test: migrate tests to Vitest and improve structure
    
    - Refactored unit and e2e tests to use Vitest instead of Jest.
    - Improved test descriptions and structure for better readability.
    - Updated fetch-mock usage to align with Vitest's testing framework.
    
    chore: update TypeScript configuration
    
    - Changed target to ES2022 and adjusted module resolution settings.
    - Included Vite types and modified include/exclude patterns.
    
    chore: add Vite configuration for library build
    
    - Created vite.config.ts for building the library with Rollup.
    - Configured TypeScript plugin for generating type declarations.

--- test/e2e/piRestService.spec.ts
+++ test/e2e/piRestService.spec.ts
@@ -1,116 +1,139 @@
-import 'cross-fetch/polyfill';
-import {DefaultParser} from "../../src/parser/defaultParser.js";
-import {PlainTextParser} from "../../src/parser/plainTextParser.js";
-import {PiRestService} from "../../src/restservice/piRestService.js";
-import {RequestOptions} from "../../src/restservice/requestOptions.js";
-import DataRequestResult from "../../src/restservice/dataRequestResult.js";
+import 'cross-fetch/polyfill'
+import { describe, expect, it } from 'vitest'
 
-const baseUrl = process.env.TEST_URL || "";
+import { DefaultParser } from '../../src/parser/defaultParser.js'
+import { PlainTextParser } from '../../src/parser/plainTextParser.js'
+import { PiRestService } from '../../src/restservice/piRestService.js'
+import { RequestOptions } from '../../src/restservice/requestOptions.js'
+import DataRequestResult from '../../src/restservice/dataRequestResult.js'
 
-describe("pi rest service: GET", function () {
-    it("get locations", async function () {
-        const provider = new PiRestService(baseUrl);
-        const res = await provider.getData<DataRequestResult<unknown>>(baseUrl + "/rest/fewspiservice/v1/locations?documentFormat=PI_JSON");
-        expect(res.data).not.toBeNull()
+const baseUrl = process.env.DOCKER_URL || ''
 
-    })
-    it("get locations with default parser and absolute url", async function () {
-        const provider = new PiRestService(baseUrl);
-        const requestOptions = new RequestOptions();
-        requestOptions.relativeUrl = false
-        const res = await provider.getDataWithParser<DataRequestResult<unknown>>(baseUrl + "/rest/fewspiservice/v1/locations?documentFormat=PI_JSON", requestOptions, new DefaultParser());
-        expect(res.data).not.toBeNull()
-    })
-    it("get locations with default parser and relative url", async function () {
-        const provider = new PiRestService(baseUrl);
-        const requestOptions = new RequestOptions();
-        const res = await provider.getDataWithParser<DataRequestResult<unknown>>(  "/rest/fewspiservice/v1/locations?documentFormat=PI_JSON", requestOptions, new DefaultParser());
-        expect(res.data).not.toBeNull()
-    })
+describe('pi rest service: GET', function () {
+  it('get locations', async function () {
+    const provider = new PiRestService(baseUrl)
+    const res = await provider.getData<DataRequestResult<unknown>>(
+      baseUrl + '/rest/fewspiservice/v1/locations?documentFormat=PI_JSON'
+    )
+    expect(res.data).not.toBeNull()
+  })
+  it('get locations with default parser and absolute url', async function () {
+    const provider = new PiRestService(baseUrl)
+    const requestOptions = new RequestOptions()
+    requestOptions.relativeUrl = false
+    const res = await provider.getDataWithParser<DataRequestResult<unknown>>(
+      baseUrl + '/rest/fewspiservice/v1/locations?documentFormat=PI_JSON',
+      requestOptions,
+      new DefaultParser()
+    )
+    expect(res.data).not.toBeNull()
+  })
+  it('get locations with default parser and relative url', async function () {
+    const provider = new PiRestService(baseUrl)
+    const requestOptions = new RequestOptions()
+    const res = await provider.getDataWithParser<DataRequestResult<unknown>>(
+      '/rest/fewspiservice/v1/locations?documentFormat=PI_JSON',
+      requestOptions,
+      new DefaultParser()
+    )
+    expect(res.data).not.toBeNull()
+  })
 })
 
-describe("pi rest service: POST", function () {
-    it("post timeseries/edit", async function () {
-        const provider = new PiRestService(baseUrl)
-        const res = await provider.postData<DataRequestResult<unknown>>(baseUrl + "/rest/fewspiservice/v1/timeseries/edit?timeSeriesSetIndex=0&locationId=test", JSON.stringify({
-            "test": "teste2e",
-        }));
-        const c = res.data
-        expect(res.data).not.toBeNull()
-        // add expect to check if data is not undefined
-        // expect(res.data).not.toBeUndefined()
+describe('pi rest service: POST', function () {
+  it('post timeseries/edit', async function () {
+    const provider = new PiRestService(baseUrl)
+    const res = await provider.postData<DataRequestResult<unknown>>(
+      baseUrl +
+        '/rest/fewspiservice/v1/timeseries/edit?timeSeriesSetIndex=0&locationId=test',
+      JSON.stringify({
+        test: 'teste2e'
+      })
+    )
+    const c = res.data
+    expect(res.data).not.toBeNull()
+    // add expect to check if data is not undefined
+    // expect(res.data).not.toBeUndefined()
+  })
+})
+
+describe('pi rest service localhost: POST timeseries/edit', function () {
+  it('post timeseries/edit', async function () {
+    const headers = {
+      'Content-Type': 'application/json'
     }
+    const provider = new PiRestService(baseUrl)
+    const res = await provider.postData<DataRequestResult<unknown>>(
+      '/rest/fewspiservice/v1/timeseries/edit?timeSeriesSetIndex=42&locationId=test',
+      JSON.stringify({
+        test: 'teste2e'
+      }),
+      headers
     )
+    const c = res.data
+    expect(res.data).not.toBeNull()
+    // add expect to check if data is not undefined
+    // expect(res.data).not.toBeUndefined()
+  })
 })
 
-describe("pi rest service localhost: POST timeseries/edit", function () {
-    it("post timeseries/edit", async function () {
-            const headers = {
-                'Content-Type': "application/json"
-            }
-            const provider = new PiRestService(baseUrl)
-            const res = await provider.postData<DataRequestResult<unknown>>("/rest/fewspiservice/v1/timeseries/edit?timeSeriesSetIndex=42&locationId=test", JSON.stringify({
-                "test": "teste2e",
-            }), headers);
-            const c = res.data
-            expect(res.data).not.toBeNull()
-            // add expect to check if data is not undefined
-            // expect(res.data).not.toBeUndefined()
-        }
+describe('pi rest service localhost: POST timeseries in PI XML format', function () {
+  it('post timeseries/edit', async function () {
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    const piXmlContent =
+      '<?xml version="1.0" encoding="UTF-8"?>\n' +
+      '<TimeSeries xmlns="http://www.wldelft.nl/fews/PI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.wldelft.nl/fews/PI https://fewsdocs.deltares.nl/schemas/version1.0/pi-schemas/pi_timeseries.xsd" version="1.27" xmlns:fs="http://www.wldelft.nl/fews/fs">\n' +
+      '    <timeZone>0.0</timeZone>\n' +
+      '    <series>\n' +
+      '        <header>\n' +
+      '            <type>instantaneous</type>\n' +
+      '            <moduleInstanceId>Coastal_Process_Astro</moduleInstanceId>\n' +
+      '            <locationId>5028N331W</locationId>\n' +
+      '            <parameterId>H.astronomical</parameterId>\n' +
+      '            <qualifierId>HighTide</qualifierId>\n' +
+      '            <timeStep unit="nonequidistant"/>\n' +
+      '            <startDate date="2021-06-24" time="08:00:00"/>\n' +
+      '            <endDate date="2021-06-25" time="09:00:00"/>\n' +
+      '            <missVal>-999.0</missVal>\n' +
+      '            <stationName>Torquay Astro</stationName>\n' +
+      '            <lat>50.46667804024676</lat>\n' +
+      '            <lon>-3.533324439637471</lon>\n' +
+      '            <x>291274.0</x>\n' +
+      '            <y>64107.0</y>\n' +
+      '            <units>m</units>\n' +
+      '        </header>\n' +
+      '    </series>\n' +
+      '</TimeSeries>    \n'
+    const piXmlEncoded =
+      'piTimeSeriesXmlContent=' + encodeURIComponent(piXmlContent)
+    const requestOption = new RequestOptions()
+    const provider = new PiRestService(baseUrl)
+    const res = await provider.postDataWithParser<DataRequestResult<string>>(
+      '/rest/fewspiservice/v1/timeseries',
+      requestOption,
+      new PlainTextParser(),
+      piXmlEncoded,
+      headers
     )
+    expect(res.data).not.toBeNull()
+    // expect(res.data).not.toBeUndefined()
+    // expect(res.responseCode).toBe(200)
+    // expect(res.contentType).toBe("application/xml")
+  })
 })
 
-describe("pi rest service localhost: POST timeseries in PI XML format", function () {
-    it("post timeseries/edit", async function () {
-            const headers = {
-                'Content-Type': "application/x-www-form-urlencoded"
-            }
-            const piXmlContent = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                "<TimeSeries xmlns=\"http://www.wldelft.nl/fews/PI\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.wldelft.nl/fews/PI https://fewsdocs.deltares.nl/schemas/version1.0/pi-schemas/pi_timeseries.xsd\" version=\"1.27\" xmlns:fs=\"http://www.wldelft.nl/fews/fs\">\n" +
-                "    <timeZone>0.0</timeZone>\n" +
-                "    <series>\n" +
-                "        <header>\n" +
-                "            <type>instantaneous</type>\n" +
-                "            <moduleInstanceId>Coastal_Process_Astro</moduleInstanceId>\n" +
-                "            <locationId>5028N331W</locationId>\n" +
-                "            <parameterId>H.astronomical</parameterId>\n" +
-                "            <qualifierId>HighTide</qualifierId>\n" +
-                "            <timeStep unit=\"nonequidistant\"/>\n" +
-                "            <startDate date=\"2021-06-24\" time=\"08:00:00\"/>\n" +
-                "            <endDate date=\"2021-06-25\" time=\"09:00:00\"/>\n" +
-                "            <missVal>-999.0</missVal>\n" +
-                "            <stationName>Torquay Astro</stationName>\n" +
-                "            <lat>50.46667804024676</lat>\n" +
-                "            <lon>-3.533324439637471</lon>\n" +
-                "            <x>291274.0</x>\n" +
-                "            <y>64107.0</y>\n" +
-                "            <units>m</units>\n" +
-                "        </header>\n" +
-                "    </series>\n" +
-                "</TimeSeries>    \n"
-            const piXmlEncoded = "piTimeSeriesXmlContent=" + encodeURIComponent(piXmlContent);
-            const requestOption = new RequestOptions()
-            const provider = new PiRestService(baseUrl)
-            const res = await provider.postDataWithParser<DataRequestResult<string>>("/rest/fewspiservice/v1/timeseries", requestOption, new PlainTextParser(), piXmlEncoded, headers);
-            expect(res.data).not.toBeNull()
-            // expect(res.data).not.toBeUndefined()
-            // expect(res.responseCode).toBe(200)
-            // expect(res.contentType).toBe("application/xml")
-
-        }
-    )
-})
-
-describe("pi rest service localhost: POST timeseries in PI JSON format", function () {
-    it("post timeseries/edit", async function () {
-            const headers = {
-                'Content-Type': "application/x-www-form-urlencoded"
-            }
-            const diagnosticsResponse = `<?xml version="1.0" encoding="UTF-8"?>
+describe('pi rest service localhost: POST timeseries in PI JSON format', function () {
+  it('post timeseries/edit', async function () {
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    const diagnosticsResponse = `<?xml version="1.0" encoding="UTF-8"?>
 <Diag xmlns="http://www.wldelft.nl/fews/PI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.wldelft.nl/fews/PI https://fewsdocs.deltares.nl/schemas/version1.0/pi-schemas/pi_diag.xsd" version="1.2">
     <line level="3" description="Import.Info: 0 time series imported, 1 time series rejected"/>
 </Diag>`
-            const piJsonContent = `{
+    const piJsonContent = `{
               "version" : "1.23",
               "timeZone" : "0.0",
               "timeSeries" : [ {
@@ -153,16 +176,22 @@ describe("pi rest service localhost: POST timeseries in PI JSON format", functio
               } ]
             }`
 
-            const piJsonEncoded = "piTimeSeriesJsonContent=" + encodeURIComponent(piJsonContent);
-            const requestOption = new RequestOptions()
-            const provider = new PiRestService(baseUrl)
-            const res = await provider.postDataWithParser<DataRequestResult<string>>("/rest/fewspiservice/v1/timeseries", requestOption, new PlainTextParser(), piJsonEncoded, headers);
-            // The response will be xml. Get it as plain text using plain text parser.
-            expect(res.data).not.toBeNull()
-            // expect(res.data).not.toBeUndefined()
-            // expect(res.responseCode).toBe(200)
-            // expect(res.contentType).toBe("application/xml")
-            // expect(res.data).toEqual(diagnosticsResponse)
-        }
+    const piJsonEncoded =
+      'piTimeSeriesJsonContent=' + encodeURIComponent(piJsonContent)
+    const requestOption = new RequestOptions()
+    const provider = new PiRestService(baseUrl)
+    const res = await provider.postDataWithParser<DataRequestResult<string>>(
+      '/rest/fewspiservice/v1/timeseries',
+      requestOption,
+      new PlainTextParser(),
+      piJsonEncoded,
+      headers
     )
+    // The response will be xml. Get it as plain text using plain text parser.
+    expect(res.data).not.toBeNull()
+    // expect(res.data).not.toBeUndefined()
+    // expect(res.responseCode).toBe(200)
+    // expect(res.contentType).toBe("application/xml")
+    // expect(res.data).toEqual(diagnosticsResponse)
+  })
 })
--- test/unit/piRestService.spec.ts
+++ test/unit/piRestService.spec.ts
@@ -1,127 +1,201 @@
-import 'cross-fetch/polyfill';
-import fetchMock from "fetch-mock";
+import 'cross-fetch/polyfill'
+import fetchMock from 'fetch-mock'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-import {PiRestService} from "../../src/restservice/piRestService.js";
-import {PlainTextParser} from "../../src/parser/plainTextParser.js";
-import {RequestOptions} from "../../src/restservice/requestOptions.js";
+import { PiRestService } from '../../src/restservice/piRestService.js'
+import { PlainTextParser } from '../../src/parser/plainTextParser.js'
+import { RequestOptions } from '../../src/restservice/requestOptions.js'
 
 import expectedLocations from './mock/locations.json'
 
 async function transformRequest(request: Request): Promise<Request> {
-    const requestInit: RequestInit = {
-        // Only some of the properties of RequestInit are used by fetch-mock, such as 'headers'.
-        headers: { 'Content-Type': "application/json" },
-    }
-    const newRequest = new Request(request, requestInit)
-    return newRequest
+  const requestInit: RequestInit = {
+    // Only some of the properties of RequestInit are used by fetch-mock, such as 'headers'.
+    headers: { 'Content-Type': 'application/json' }
+  }
+  const newRequest = new Request(request, requestInit)
+  return newRequest
 }
 
 async function transformRequestWithToken(request: Request): Promise<Request> {
-    const requestInit: RequestInit = {
-        // Only some of the properties of RequestInit are used by fetch-mock, such as 'headers'.
-        headers: {
-            'Content-Type': "application/json",
-            'Authorization': "Bearer testtoken"
-        },
+  const requestInit: RequestInit = {
+    // Only some of the properties of RequestInit are used by fetch-mock, such as 'headers'.
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer testtoken'
     }
-    const newRequest = new Request(request, requestInit)
-    return newRequest
+  }
+  const newRequest = new Request(request, requestInit)
+  return newRequest
 }
 
-const baseUrl = process.env.TEST_URL || "";
+const baseUrl = process.env.TEST_URL || ''
 
-describe("pi rest service: GET", function () {
+describe('pi rest service: GET', function () {
+  beforeAll(() => fetchMock.mockGlobal())
 
-    afterAll(function () {
-        fetchMock.restore();
-    });
+  afterAll(() => {
+    fetchMock.hardReset()
+  })
 
-    it("Get Locations", async function () {
-        fetchMock.get("https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations?documentFormat=PI_JSON", {
-            status: 200,
-            body: JSON.stringify(expectedLocations)
-        });
-        const provider = new PiRestService(baseUrl, transformRequestWithToken)
-        const res = await provider.getData("https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations?documentFormat=PI_JSON" )
-        expect(res.data).not.toBeNull();
-        expect(res.data).toStrictEqual(expectedLocations);
-        expect(res.responseCode).toStrictEqual(200)
-        expect(fetchMock.lastCall()?.request?.headers.get('Content-Type')).toBe("application/json")
-        const transformProvider = new PiRestService(baseUrl, transformRequest)
-        const resWithTransformedRequest = await transformProvider.getData("https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations?documentFormat=PI_JSON" )
-        expect(resWithTransformedRequest.data).not.toBeNull();
-        expect(fetchMock.lastCall()?.request?.headers.get('Content-Type')).toBe("application/json")
-        expect(resWithTransformedRequest.data).toStrictEqual(expectedLocations);
-    })
-    it("Get Locations Not Found", async function () {
-        const mockReponse =  {
-            status: 400, body: JSON.stringify("not found")
-        }
-        fetchMock.get("https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations", mockReponse);
-        const provider = new PiRestService(baseUrl)
-        const res = provider.getData("https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations")
-        
-        const expectedError = new Error("Fetch Error", {cause: mockReponse})
-        await expect(res).rejects.toThrow(expectedError);
-    })
-    it("Get Locations Invalid JSON response", async function () {
-        fetchMock.get("https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations?invalid", {
-            status: 200, body: "{"
-        });
-        const provider = new PiRestService(baseUrl)
-        const res = provider.getData("https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations?invalid")
-        await expect(res).rejects.toThrow("Parse Error for response https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations?invalid");
-    });
+  it('Get Locations', async function () {
+    fetchMock.route(
+      'https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations?documentFormat=PI_JSON',
+      {
+        status: 200,
+        body: JSON.stringify(expectedLocations)
+      }
+    )
+    const provider = new PiRestService(baseUrl, transformRequestWithToken)
+    const res = await provider.getData(
+      'https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations?documentFormat=PI_JSON'
+    )
+    expect(res.data).not.toBeNull()
+    expect(res.data).toStrictEqual(expectedLocations)
+    expect(res.responseCode).toStrictEqual(200)
+    expect(
+      fetchMock.callHistory.lastCall()?.request?.headers.get('Content-Type')
+    ).toBe('application/json')
+    const transformProvider = new PiRestService(baseUrl, transformRequest)
+    const resWithTransformedRequest = await transformProvider.getData(
+      'https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations?documentFormat=PI_JSON'
+    )
+    expect(resWithTransformedRequest.data).not.toBeNull()
+    expect(
+      fetchMock.callHistory.lastCall()?.request?.headers.get('Content-Type')
+    ).toBe('application/json')
+    expect(resWithTransformedRequest.data).toStrictEqual(expectedLocations)
+  })
+  it('Get Locations Not Found', async function () {
+    const mockReponse = {
+      status: 400,
+      body: JSON.stringify('not found')
+    }
+    fetchMock.route(
+      'https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations',
+      mockReponse
+    )
+    const provider = new PiRestService(baseUrl)
+    try {
+      const res = provider.getData(
+        'https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations'
+      )
+      await expect(res).rejects.toThrow('Fetch Error');
+    } catch (error: any) {
+      expect(error).toBeInstanceOf(Error)
+      expect(error.message).toBe('Fetch Error')
+
+      // The cause will be a Response
+      const response = error.cause as Response
+      expect(response).toBeInstanceOf(Response)
+      expect(response.status).toBe(400)
+
+      const text = await response.text()
+      expect(text).toBe('"not found"')
+    }
+  })
+  it('Get Locations Invalid JSON response', async function () {
+    fetchMock.route(
+      'https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations?invalid',
+      {
+        status: 200,
+        body: '{'
+      }
+    )
+    const provider = new PiRestService(baseUrl)
+    const res = provider.getData(
+      'https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations?invalid'
+    )
+    await expect(res).rejects.toThrow(
+      'Parse Error for response https://mock.dev/fewswebservices/rest/fewspiservice/v1/locations?invalid'
+    )
+  })
 })
 
-describe("pi rest service json body: POST", function () {
+describe('pi rest service json body: POST', function () {
+  beforeAll(() => {
+    fetchMock.mockGlobal()
 
-    beforeAll(function () {
-        fetchMock.post("https://mock.dev/fewswebservices/rest/fewspiservice/v1/timeseries/edit", {
-          status: 200,
-          body: JSON.stringify({ "responseCode": 200 })
-        });
-      });
+    fetchMock.post(
+      'https://mock.dev/fewswebservices/rest/fewspiservice/v1/timeseries/edit',
+      {
+        status: 200,
+        body: JSON.stringify({ responseCode: 200 })
+      }
+    )
+  })
 
-    it("Post timeseries/edit", async function () {
-        const provider = new PiRestService(baseUrl)
-        const res = await provider.postData("https://mock.dev/fewswebservices/rest/fewspiservice/v1/timeseries/edit", JSON.stringify({ "test": "test" }))
-        expect(res.data).not.toBeNull()
-        expect(res.data).toStrictEqual({ "responseCode": 200 })
-        // check if headers are equal to default
-        expect(fetchMock.lastCall()?.request?.headers.get('Content-Type')).toBe("application/json")
-    })
+  afterAll(() => {
+    fetchMock.hardReset()
+  })
 
-    it("Post timeseries/edit with given headers", async function () {
-        const provider = new PiRestService(baseUrl)
-        const headers = {
-            'Content-Type': "application/ld+json"
-        }
-        const res = await provider.postData("https://mock.dev/fewswebservices/rest/fewspiservice/v1/timeseries/edit", JSON.stringify({ "test": "test" }), headers)
-        expect(res.data).not.toBeNull()
-        expect(res.data).toStrictEqual({ "responseCode": 200 })
-        expect(fetchMock.lastCall()?.request?.headers.get('Content-Type')).toBe("application/ld+json")
-    })
+  it('Post timeseries/edit', async function () {
+    const provider = new PiRestService(baseUrl)
+    const res = await provider.postData(
+      'https://mock.dev/fewswebservices/rest/fewspiservice/v1/timeseries/edit',
+      JSON.stringify({ test: 'test' })
+    )
+    expect(res.data).not.toBeNull()
+    expect(res.data).toStrictEqual({ responseCode: 200 })
+    // check if headers are equal to default
+    expect(
+      fetchMock.callHistory.lastCall()?.request?.headers.get('Content-Type')
+    ).toBe('application/json')
+  })
+
+  it('Post timeseries/edit with given headers', async function () {
+    const provider = new PiRestService(baseUrl)
+    const headers = {
+      'Content-Type': 'application/ld+json'
+    }
+    const res = await provider.postData(
+      'https://mock.dev/fewswebservices/rest/fewspiservice/v1/timeseries/edit',
+      JSON.stringify({ test: 'test' }),
+      headers
+    )
+    expect(res.data).not.toBeNull()
+    expect(res.data).toStrictEqual({ responseCode: 200 })
+    expect(
+      fetchMock.callHistory.lastCall()?.request?.headers.get('Content-Type')
+    ).toBe('application/ld+json')
+  })
 })
 
+describe('pi rest service json urlencoded: POST', function () {
+  beforeAll(() => {
+    fetchMock.mockGlobal()
+  })
 
-describe("pi rest service json urlencoded: POST", function () {
+  afterAll(() => {
+    fetchMock.hardReset()
+  })
 
-    it("Post timeseries url encoded json post with text parser", async function () {
-        fetchMock.post("https://mock.dev/fewswebservices/rest/fewspiservice/v1/timeseries", {
-            status: 200,
-            body: "{}"
-        });
-        const provider = new PiRestService(baseUrl)
-        const headers = {
-            'Content-Type': "application/xml"
-        }
-        const requestOption = new RequestOptions()
-        requestOption.relativeUrl = false
-        const res = await provider.postDataWithParser("https://mock.dev/fewswebservices/rest/fewspiservice/v1/timeseries", requestOption, new PlainTextParser(), JSON.stringify({"test": "test"}), headers)
-        expect(res.data).not.toBeNull()
-        expect(res.data).toStrictEqual("{}")
-        expect(res.responseCode).toBe(200)
-        expect(fetchMock.lastCall()?.request?.headers.get('Content-Type')).toBe("application/xml")
-    })
+  it('Post timeseries url encoded json post with text parser', async function () {
+    fetchMock.post(
+      'https://mock.dev/fewswebservices/rest/fewspiservice/v1/timeseries',
+      {
+        status: 200,
+        body: '{}'
+      }
+    )
+    const provider = new PiRestService(baseUrl)
+    const headers = {
+      'Content-Type': 'application/xml'
+    }
+    const requestOption = new RequestOptions()
+    requestOption.relativeUrl = false
+    const res = await provider.postDataWithParser(
+      'https://mock.dev/fewswebservices/rest/fewspiservice/v1/timeseries',
+      requestOption,
+      new PlainTextParser(),
+      JSON.stringify({ test: 'test' }),
+      headers
+    )
+    expect(res.data).not.toBeNull()
+    expect(res.data).toStrictEqual('{}')
+    expect(res.responseCode).toBe(200)
+    expect(
+      fetchMock.callHistory.lastCall()?.request?.headers.get('Content-Type')
+    ).toBe('application/xml')
+  })
 })
